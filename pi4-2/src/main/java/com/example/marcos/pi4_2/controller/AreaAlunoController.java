e7c8beab75c95f6193eee032571629a94737bf6c
97Mith <136820655+97Mith@users.noreply.github.com>
2024-11-26 09:06:32 -0300

    criandopáginas da área aluno

--- pi4-2/src/main/java/com/example/marcos/pi4_2/controller/AreaAlunoController.java
+++ pi4-2/src/main/java/com/example/marcos/pi4_2/controller/AreaAlunoController.java
@@ -2,9 +2,11 @@ package com.example.marcos.pi4_2.controller;
 
 import com.example.marcos.pi4_2.services.AulaService;
 import com.example.marcos.pi4_2.entities.Aula;
+import com.example.marcos.pi4_2.services.FaturaAlunoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
@@ -14,10 +16,12 @@ import java.util.List;
 public class AreaAlunoController {
 
     private final AulaService aulaService;
+    private final FaturaAlunoService faturaAlunoService;
 
     @Autowired
-    public AreaAlunoController(AulaService aulaService) {
+    public AreaAlunoController(AulaService aulaService, FaturaAlunoService faturaAlunoService) {
         this.aulaService = aulaService;
+        this.faturaAlunoService = faturaAlunoService;
     }
 
     @GetMapping("/feed")
@@ -25,5 +29,11 @@ public class AreaAlunoController {
         List<Aula> aulas = aulaService.findAll();
         model.addAttribute("aulas", aulas);
         return "areaaluno/alunohome";  // Retorna o template correto
+    }
+
+    @GetMapping("/minhas-faturas")
+    public String minhasFaturas(ModelMap model){
+        model.addAttribute("faturas", faturaAlunoService.findAll());
+        return "/areaaluno/listafaturas";
     }
 }
